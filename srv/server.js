1deb397f5a8ec2d3537b7492245d1f1114a362ae
SHANKARI G R <shankari.g.r@sap.com>
2021-11-12 15:39:35 +0530

    VCAP changes

--- srv/server.js
+++ srv/server.js
@@ -2,8 +2,10 @@ const cds = require('@sap/cds')
 const emutil = require("./em");
 // react on bootstrapping events...
 cds.on('served', ()=>{
+    console.log("VCAP_SERVICE------->",process.env.VCAP_SERVICES);
     const vcap = JSON.parse(process.env.VCAP_SERVICES);
     const emjson = vcap["enterprise-messaging"]
+    // console.log("emjson------->",emjson);
     const credentials = emjson[0].credentials
     emutil.emConfiguration(credentials);
 });
